75595b8f06ce84fe0c9f3a1b80531b87250f3596
Preston Van Loon <preston@prysmaticlabs.com>
2019-06-25 12:24:58 -0400

    handle special edge case for domain

--- yaml-hex-to-base64/hex-to-base64.ts
+++ yaml-hex-to-base64/hex-to-base64.ts
@@ -1,11 +1,22 @@
 import { Buffer as ImportedBuffer } from "buffer";
 
-const re = RegExp(/'0x[a-fA-F0-9]*'/, 'g');
+// Matching any line with a hex string.
+const outer = RegExp(/^.*0x[a-fA-F0-9]*.*/, 'gm');
+
+// Matching the hex string itself.
+const inner = RegExp(/0x[a-fA-F0-9]*/, 'g');
 
 export function yamlHexToBase64(input: string): string {
-  return input.replace(re, (match) => {
+  return input.replace(outer, (match) => {
     match = match.replace(/'/g, '');
-    return `'${Buffer.from(match.slice(2), 'hex').toString('Base64')}'`;
+
+    // Special case: domain fields are supposed to be hexadecimal.
+    if (match.includes('domain')) {
+      return match;
+    }
+    return match.replace(inner, (match) => {
+      return `${Buffer.from(match.slice(2), 'hex').toString('Base64')}`;
+    });
   });
 }
 
